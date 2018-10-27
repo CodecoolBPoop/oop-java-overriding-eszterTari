f7c1a896a35837e555a7c85d1eafd4371ec1bcc7
eszterTari <tari.eszter.eng@gmail.com>
2018-10-27 16:25:25 +0200

    class is implemented with tests

--- src/com/codecool/uml/overriding/Webshop.java
+++ src/com/codecool/uml/overriding/Webshop.java
@@ -3,6 +3,21 @@ package com.codecool.uml.overriding;
 public class Webshop {
 
     public static void main(String[] args) {
-	// write your code here
+        Order order = new Order();
+        CheckoutProcess checkoutProcess = new CheckoutProcess();
+        PaymentProcess paymentProcess = new PaymentProcess();
+
+        System.out.println("Test for steps in right sequence:");
+        checkoutProcess.process(order);
+        paymentProcess.process(order);
+        System.out.println("Test is finished!");
+        System.out.println("");
+
+
+        System.out.println("Test for steps in wrong sequence:");
+        paymentProcess.process(order);
+        checkoutProcess.process(order);
+        System.out.println("Test is finished!");
+        System.out.println("");
     }
 }
